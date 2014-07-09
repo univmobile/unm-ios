9b90dfb7c5748dd0d57a527d2a13c636d904c118
David <david.andriana@avantage-compris.com>
2014-07-09 07:16:49 +0200

    Clean up: Add author full name in comments

--- UnivMobile/main.m
+++ UnivMobile/main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  UnivMobile
 //
-//  Created by David on 03/07/2014.
-//  Copyright (c) 2014 David. All rights reserved.
+//  Created by David Andrianavalontsalama on 03/07/2014.
+//  Copyright (c) 2014 UNPIdF. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
