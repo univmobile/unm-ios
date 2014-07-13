99d8058d670c03675e403ee7283724ceb3383964
David <david.andriana@avantage-compris.com>
2014-07-13 14:26:24 +0200

    Appium tests: Now swiping to/from the About Page

--- unm-ios-it/src/test/java/fr/univmobile/ios/it/HomePageTest.java
+++ unm-ios-it/src/test/java/fr/univmobile/ios/it/HomePageTest.java
@@ -11,5 +11,21 @@ public class HomePageTest extends AbstractUnivMobileAppiumTest {
 		takeScreenshot("home.png");
 
 		savePageSource("pageSource.xml");
+
+		// Don’t ask me why one only swipe doesn’t work.
+
+		swipe(80, 168, 77, 414, 800);
+		swipe(80, 168, 77, 414, 800);
+		swipe(80, 168, 77, 414, 800);
+
+		takeScreenshot("about.png");
+
+		// Don’t ask me why one only swipe doesn’t work.
+
+		swipe(80, 414, 77, 168, 800);
+		swipe(80, 414, 77, 168, 800);
+		swipe(80, 414, 77, 168, 800);
+
+		takeScreenshot("home2.png");
 	}
 }
