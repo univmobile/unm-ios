2b6b7bd1ce8a06d301b35de949a4028f53f82453
David <david.andriana@avantage-compris.com>
2014-08-04 01:22:22 +0200

    Refactor: NSBundle+String has its own .h and .m files

---
+++ UnivMobile/Utilities/NSBundle+String.h
@@ -0,0 +1,13 @@
+//
+//  NSBundle+String.h
+//  UnivMobile
+//
+//  Created by David on 04/08/2014.
+//  Copyright (c) 2014 David. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSBundle (String)
+
+@end
