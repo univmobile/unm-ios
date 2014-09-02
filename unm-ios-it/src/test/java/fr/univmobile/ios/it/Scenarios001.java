b142bae41da1bf36aa89e9cb6742878d5c38e42f
David <david.andriana@avantage-compris.com>
2014-09-02 02:59:33 +0200

    Add scenario: "Géocampus_001, commentaires"

--- unm-ios-it/src/test/java/fr/univmobile/ios/it/Scenarios001.java
+++ unm-ios-it/src/test/java/fr/univmobile/ios/it/Scenarios001.java
@@ -196,8 +196,8 @@ public class Scenarios001 extends AppiumIOSEnabledTest {
 
 		savePageSource("geocampus_list.xml");
 
-		//getDriver().findElement(By.xpath("//UIATabBar[1]/UIAButton[2]"))
-			//	.click();
+		// getDriver().findElement(By.xpath("//UIATabBar[1]/UIAButton[2]"))
+		// .click();
 		elementByXPath("//UIATabBar[1]/UIAButton[2]").click();
 		// elementById("Plan").click();
 
@@ -222,5 +222,27 @@ public class Scenarios001 extends AppiumIOSEnabledTest {
 		pause(PAUSE);
 
 		takeScreenshot("geocampus_back_to_map.png");
+	}
+
+	@Scenario("Géocampus, commentaires")
+	@Test
+	public void Geocampus_001() throws Exception {
+
+		pause(PAUSE);
+		takeScreenshot("home.png");
+		elementById("button-Géocampus").click();
+		pause(PAUSE);
+		takeScreenshot("geocampus_list.png");
+		elementByXPath("//UIATabBar[1]/UIAButton[2]").click();
+		// elementByName("Plan").click();
+		pause(PAUSE);
+		takeScreenshot("geocampus_map.png");
+		elementById("label-poi-name").click();
+		pause(PAUSE);
+		takeScreenshot("geocampus_details.png");
+		elementByXPath("//UIATabBar[1]/UIAButton[2]").click();
+		// elementByName("Commentaires").click();
+		pause(PAUSE);
+		takeScreenshot("comments.png");
 	}
 }
