78630b537a5e7dad9af9e5297c7a3c4c17a6af17
David <david.andriana@avantage-compris.com>
2014-09-10 00:26:50 +0200

    Refactor: Use a JDBC MySQL DataSource in lieu of comment_threads/ XML files

--- unm-ios-it/src/test/java/fr/univmobile/ios/it/Scenarios001.java
+++ unm-ios-it/src/test/java/fr/univmobile/ios/it/Scenarios001.java
@@ -1,6 +1,10 @@
 package fr.univmobile.ios.it;
 
+import static fr.univmobile.backend.core.impl.ConnectionType.MYSQL;
+
 import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +15,7 @@ import fr.univmobile.it.commons.DeviceNames;
 import fr.univmobile.it.commons.EnvironmentUtils;
 import fr.univmobile.it.commons.Scenario;
 import fr.univmobile.it.commons.Scenarios;
+import fr.univmobile.testutil.PropertiesUtils;
 
 @Scenarios("Scénarios simples")
 @DeviceNames({ "iPhone Retina (3.5-inch)", "iPhone Retina (4-inch)" })
@@ -23,7 +28,17 @@ public class Scenarios001 extends AppiumIOSEnabledTest {
 		final String dataDir = TestBackend.readBackendAppDataDir(new File(
 				"target", "unm-backend-app-noshib/WEB-INF/web.xml"));
 
-		TestBackend.setUpData("001", new File(dataDir));
+		final Connection cxn = DriverManager.getConnection(
+				PropertiesUtils.getTestProperty("mysqlUrl"),
+				PropertiesUtils.getTestProperty("mysqlUsername"),
+				PropertiesUtils.getTestProperty("mysqlPassword"));
+		try {
+
+			TestBackend.setUpData("001", new File(dataDir), MYSQL, cxn);
+
+		} finally {
+			cxn.close();
+		}
 	}
 
 	@Scenario("Aller-retour sur la page « À Propos »")
